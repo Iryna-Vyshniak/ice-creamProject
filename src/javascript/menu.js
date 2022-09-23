bda7b035d67648437bf5e592842c35d792767c60
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-09-24 02:27:21 +0300

    add icons, add script to mobile-menu, create adaptive header

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -1,21 +1,36 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('.menu-open'),
-    closeMenuBtn: document.querySelector('.menu-close'),
-    menu: document.querySelector('.mobile-menu'),
-    menuList: document.querySelector('.mobile-menu__list'),
-  };
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open'),
+//     closeMenuBtn: document.querySelector('.menu-close'),
+//     menu: document.querySelector('.mobile-menu'),
+//     menuList: document.querySelector('.mobile-menu__list'),
+//   };
   
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  refs.menuList.addEventListener('click', removeMenu);
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+//   refs.menuList.addEventListener('click', removeMenu);
 
-  function toggleMenu() {
-    document.body.classList.toggle('no-scroll');
-    refs.menu.classList.toggle('is-hidden');
-  }
-  function removeMenu() {
-    refs.menu.classList.add('is-hidden');
-    document.body.classList.remove('no-scroll');
-  }
+//   function toggleMenu() {
+//     document.body.classList.toggle('no-scroll');
+//     refs.menu.classList.toggle('is-hidden');
+//   }
+//   function removeMenu() {
+//     refs.menu.classList.add('is-hidden');
+//     document.body.classList.remove('no-scroll');
+//   }
+// })();
+
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
