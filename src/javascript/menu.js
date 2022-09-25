d0a662cfd51fa7f7927ee81accf483803274c585
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-09-25 22:46:19 +0300

    add adaptive hero & header

--- src/javascript/menu.js
+++ src/javascript/menu.js
@@ -19,18 +19,3 @@
     document.body.classList.remove('no-scroll');
   }
 })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
