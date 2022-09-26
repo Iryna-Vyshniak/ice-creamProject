02eccaaff168ded390c6280cbb71957762c53944
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-09-26 22:17:06 +0300

    modified files

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -2,19 +2,19 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     openMobileBtn: document.querySelector('[data-mobile-open]'),
-    openModalReadBtn: document.querySelector('[data-modal-read-open]'),
+    // openModalReadBtn: document.querySelector('[data-modal-read-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     closeMobileBtn: document.querySelector('[data-mobile-close]'),
-    closeModalReadBtn: document.querySelector('[data-modal-read-close]'),
+    // closeModalReadBtn: document.querySelector('[data-modal-read-close]'),
     modal: document.querySelector('[data-modal]'),
-    modal: document.querySelector('[data-read-modal]'),
+    // modal: document.querySelector('[data-read-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openMobileBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.closeMobileBtn.addEventListener('click', toggleModal);
-  refs.closeModalReadBtn.addEventListener('click', toggleModal);
+  // refs.closeModalReadBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
